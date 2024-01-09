e2352e9679c39171577941df31146750cb962315
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-01-09 23:49:56 +0100

    gérer le score

--- azertype/Scripts/script.js
+++ azertype/Scripts/script.js
@@ -4,40 +4,47 @@ function afficherProposition(proposition){
 }
 
 
-
+let motSaisi
 function lancerJeu() {
 // Initialisation
     let score = 0
-    let nbMotsProposés = 0  
+     
     let i = 0
     let btnValiderMot = document.getElementById("btnValiderMot")
     let inputEcriture = document.getElementById("inputEcriture")
     
     afficherProposition(listeMots[i])
-    btnValiderMot.addEventListener("click",()=>{
+   btnValiderMot.addEventListener("click",()=>{
 
+        if(inputEcriture.value === listeMots[i]){
+            score++
+            console.log(score)
+        }
+        
         //Affichage de ma liste de mots dans la zone affichage
         i++
+        
+        // afficherResultat(score,i)
+        //On efface la zone de saisie
         inputEcriture.value = "" 
         // lorsque la valeur est indéfini à la fin de ma liste de mots, j'affiche la fin du jeu
+       
+        
         if(listeMots[i] === undefined){
             afficherProposition ("Le jeu est fini")
             // je désactive le bouton valider    
             btnValiderMot.disabled = true
-            console.log(btnValiderMot);
+            
         } else {
             afficherProposition(listeMots[i])
-                         
+            
         } 
-
+    
         
     })
     
-    
 
-
-
-    //  afficherResultat(score,nbMotsProposés);
+    //   afficherResultat(score,i);
  
 }
 
