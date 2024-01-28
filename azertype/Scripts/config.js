9b7353cff88fd77ff66c4606cb3da251cfeb21ed
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-01-28 22:04:52 +0100

    rectification et récupération code

--- azertype/Scripts/config.js
+++ azertype/Scripts/config.js
@@ -1,3 +1,10 @@
-//Liste des mots et phrases utilisés dans le jeu
-const listeMots = ["Cachalot","Pétunia","Serviette"];
-const listePhrases = ["Pas de panique!","La vie,l'univers et le reste", "Merci pour le poisson"]
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
--- azertype/Scripts/main.js
+++ azertype/Scripts/main.js
@@ -4,8 +4,9 @@
  * 
  *********************************************************************************/
 
-// Ici, j'ai mis la fonction lancerJeu() en commentaire pour ne pas être dérangé par l'affichage des popups. 
 lancerJeu()
+
+// J'ai mis ce code en commentaire, nous pourrons le récupérer lorsque nous en auront besoin :
 
 // let inputEcriture = document.getElementById("inputEcriture")
 // console.log(inputEcriture)
@@ -16,7 +17,8 @@ lancerJeu()
 // let zoneProposition = document.querySelector(".zoneProposition")
 // console.log(zoneProposition)
 
-
+// let spanScore = document.querySelector(".zoneScore span")
+// console.log(spanScore)
 
 // let listeBtnRadio = document.querySelectorAll(".optionSource input")
 // console.log(listeBtnRadio)
